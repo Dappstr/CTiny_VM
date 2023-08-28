8ce16819c57b2d8258563d87c65281de8695e0bd
Lane Beals <lanebeals02@gmail.com>
2023-08-27 19:35:48 -0700

    Changing to one source file for now

--- src/main.c
+++ src/main.c
@@ -2,7 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../include/fileutil.h"
+unsigned int get_file_size(FILE *file) {
+    if(!file) {
+        printf("Failed to get file pointer in %s", __func__);
+        exit(1);
+    }
+    fseek(file, 0, SEEK_END);
+    unsigned int size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if(size < 1) {
+        printf("Failed to get file size\n");
+        exit(1);
+    }
+    else if(size == 1) {
+        printf("File contains only EOF\n");
+        exit(2);
+    }
+
+    return size;
+}
+
+char* get_file_contents(const char* path) {
+    FILE* file = fopen(path, "r");
+    if(!file) {
+        printf("Failed to create file object in %s", __func__);
+        exit(0);
+    }
+    
+    unsigned int size = get_file_size(file);
+    char* contents = malloc(size + 1);
+    fread(contents, 1, size, file);
+    contents[size] = '\0';
+
+    return contents;
+}
 
 void print_usage(char* argv) {
     printf("USAGE: %s <path_to_file>\n", argv);
@@ -20,6 +54,7 @@ int main(int argc, char* argv[]) {
     printf("Read: \n%s", contents);
 
     //lex
+    //char** lexemes = lex(contents);
 
     //tokenize
 
